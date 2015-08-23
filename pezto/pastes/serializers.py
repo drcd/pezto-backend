d5494d9fc9de5ee6f655a7b135d309dd30f5acec
Younes Zakaria <drcooldudealt@gmail.com>
2015-08-24 01:26:40 +0200

    Removed/Changed outdated files

--- pezto/pastes/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from pastes.models import Paste
-
-
-class PasteSerializer(serializers.Serializer):
-    class Meta:
-        model = Paste
-        fields = ('title', 'content', 'date', 'url')
