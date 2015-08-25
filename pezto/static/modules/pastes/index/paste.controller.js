223237937d863e91756df0f862bfa0e63af3886b
Stenkilde <hello@stenkil.de>
2015-08-25 23:19:07 +0100

    added foundation, also added redirect after creation

--- pezto/static/modules/pastes/index/paste.controller.js
+++ pezto/static/modules/pastes/index/paste.controller.js
@@ -6,7 +6,7 @@
 		.controller('Pasto', Pasto);
 
 	/* @ngInject */
-	function Pasto(PasteFactory, $state, $stateParams) {
+	function Pasto(PasteFactory, $state) {
 		/*jshint validthis: true */
 		var vm 			= this;
 		vm.makePaste	= makePaste;
@@ -24,6 +24,7 @@
 				content: vm.content
 			}).then(function(result) {
 				console.log(result)
+				$state.go('application.pasteview', {id: result.data.uid });
 			});
 		}
 
