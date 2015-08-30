450b3b1bf33e28a70377fdf93cc4ee1eae961819
Stenkilde <hello@stenkil.de>
2015-08-30 17:56:45 +0100

    Did a lot of work on the gruntfile, now we can compile scss and run the python server with one command

---
+++ pezto/Gruntfile.js
@@ -0,0 +1,47 @@
+// Gruntfile.js
+
+module.exports = function(grunt) {
+
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'static/styles/main.css': 'static/styles/main.scss'
+                }
+            }
+        },
+        shell: {
+            target: {
+                command: 'python manage.py runserver'
+            }
+        },
+        watch: {
+            css: {
+                files: ['static/styles/main.scss'],
+                tasks: ['sass']
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            tasks: ['watch', 'shell']
+        }
+    });
+
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-shell');
+
+    grunt.registerTask('serve', ['sass', 'concurrent']);
+};
